c4788429740a49f96bc364f31d6973c2628527b8
Raymond Gigliotti <ray@firestitch.com>
2023-09-19 13:31:48 -0400

    moved reload button beside actions

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
-import { FsExampleModule } from '@firestitch/example';
-import { FsMessageModule } from '@firestitch/message';
-import { FsFilterModule } from '@firestitch/filter';
 import { FsDatePickerModule } from '@firestitch/datepicker';
+import { FsExampleModule } from '@firestitch/example';
+import { ButtonStyle, FsFilterModule } from '@firestitch/filter';
+import { FsMessageModule } from '@firestitch/message';
 import { FsScrollbarModule } from '@firestitch/scrollbar';
 
 import { ToastrModule } from 'ngx-toastr';
@@ -15,19 +15,24 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './material.module';
 
-import { ExamplesComponent, NofiltersComponent, KitchenSinkComponent } from './components';
-import { DialogExampleComponent } from './components/dialog-example';
-import { DialogComponent } from './components/dialog';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { FsFormModule } from '@firestitch/form';
 import { FsFileModule } from '@firestitch/file';
+import { FsFormModule } from '@firestitch/form';
+import { ExamplesComponent, KitchenSinkComponent, NofiltersComponent } from './components';
+import { DialogComponent } from './components/dialog';
+import { DialogExampleComponent } from './components/dialog-example';
 
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
-    FsFilterModule.forRoot(),
+    FsFilterModule.forRoot({
+      button: {
+        style: ButtonStyle.Flat,
+        label: '',
+      },
+    }),
     FsDatePickerModule.forRoot(),
     BrowserAnimationsModule,
     AppMaterialModule,
@@ -38,18 +43,16 @@ import { FsFileModule } from '@firestitch/file';
     FsMessageModule.forRoot(),
     ToastrModule.forRoot({ preventDuplicates: true }),
     RouterModule.forRoot([
-    { path: '', component: ExamplesComponent, pathMatch: 'full' },
-    { path: 'nofilters', component: NofiltersComponent },
-    { path: 'dialog', component: DialogExampleComponent }
-], { relativeLinkResolution: 'legacy' }),
+      { path: '', component: ExamplesComponent, pathMatch: 'full' },
+      { path: 'nofilters', component: NofiltersComponent },
+      { path: 'dialog', component: DialogExampleComponent }
+    ],
+      { relativeLinkResolution: 'legacy' }),
     FsFileModule.forRoot({
       allowDownload: true,
       allowRemove: true,
       dragoverMessage: true,
     }),
-  ],
-  entryComponents: [
-    DialogComponent
   ],
   declarations: [
     AppComponent,
