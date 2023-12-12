d89503e79706bee1f1e73807bcb6f0786871fa12
Eugene Kubesh <codbasters@gmail.com>
2023-12-12 23:49:48 +0100

    SP-T2906 remove strange proxy behaviour for autocomplete chips item

--- src/app/components/filters-item/autocompletechips/autocompletechips.component.ts
+++ src/app/components/filters-item/autocompletechips/autocompletechips.component.ts
@@ -18,23 +18,11 @@ import { BaseItemComponent } from '../base-item/base-item.component';
 })
 export class AutocompletechipsComponent extends BaseItemComponent<AutocompleteChipsItem> {
 
-  public chipBackground: string;
-  public chipColor: string;
-  public chipIcon: string;
-
   constructor(
     protected _kvDiffers: KeyValueDiffers,
     protected _cd: ChangeDetectorRef,
   ) {
     super(_kvDiffers, _cd);
-  }
-
-  // SP-T1747
-  public clicked() {
-    this.chipBackground = this.item.chipBackground;
-    this.chipColor = this.item.chipColor;
-    this.chipIcon = this.item.chipIcon;
-    this._cd.markForCheck();
   }
 
   public addAutocompleteChipItem(event) {
